150ff5a3e7ea22df3845e305394d7acbf7420a6b
Trpal <jonikorpelin@gmail.com>
2018-05-08 20:01:40 +0300

    added edit expense functionality and testing

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -30,11 +30,21 @@ export const removeExpense = ({ id } = {}) => ({
 });
 
 
+// EDIT EXPENSE
+
 export const editExpense = (id, updates) => ({
 	type: 'EDIT_EXPENSE',
 	id,
 	updates
 });
+
+export const startEditExpense = (id, updates) => {
+	return (dispatch) => {
+		return database.ref(`expenses/${id}`).update(updates).then(() => {
+			dispatch(editExpense(id, updates));
+		});
+	};
+};
 
 // SET_EXPENSES
 
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -4,6 +4,7 @@ import {
 	startAddExpense,
 	addExpense,
 	editExpense,
+	startEditExpense,
 	removeExpense,
 	setExpenses,
 	startSetExpenses,
@@ -45,6 +46,24 @@ test('should setup add expense action object with provided values', () => {
 	expect(action).toEqual({
 		type: 'ADD_EXPENSE',
 		expense: expenses[2]
+	});
+});
+
+test('should edit expense from firebase', () => {
+	const store = createMockStore({});
+	const id = expenses[0].id;
+	const updates = { amount: 21045 };
+	store.dispatch(startEditExpense(id, updates)).then(() => {
+		const actions = store.getActions();
+		expect(actions[0]).toEqual({
+			type: 'EDIT_EXPENSE',
+			id,
+			updates
+		});
+		return database.ref(`expenses/${id}`).once('value');
+	}).then((snapshot) => {
+		expect(snapshot.val().amount).toBe(updates.amount);
+		done();
 	});
 });
 
